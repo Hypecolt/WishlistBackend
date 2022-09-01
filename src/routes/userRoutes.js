81557d2954f9b71665ece72ffc021d85091d2c16
Antonaru Vlad <ant.alex97@yahoo.com>
2022-09-01 18:52:45 +0300

    Fixed login/register routes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -12,7 +12,7 @@ router.get("/", authMiddleware, urlencodedParser, usersController.getUser);
 router.put("/", authMiddleware, urlencodedParser, usersController.updateUser);
 router.delete("/", authMiddleware, usersController.deleteUser);
 
-// router.get("/login", urlencodedParser, usersController.login);
-// router.post("/register", urlencodedParser, usersController.register);
+router.get("/login", urlencodedParser, usersController.login);
+router.post("/register", urlencodedParser, usersController.register);
 
 module.exports = router;
